575efa11b35e6596c46d54cd821280595df99c68
Suresh <sunkara.suresh@providence.org>
2021-01-10 01:00:15 +0530

    DB Q III set is symboles removed

--- FlashCards.bat
+++ FlashCards.bat
@@ -1,3 +1,3 @@
-start microsoft-edge:http://127.0.0.1:5000/login
-"D:\ProgramData\Anaconda3\envs\FalshCards\python.exe" "D:\AZ900-Certification-Preparation\AzureCert-flash-cards\flash_cards.py" 
+start microsoft-edge:http://127.0.0.1:8080/login
+python "C:\Users\sunkara.suresh\repos\AZ400-Certification-Preparation\AzureCert-flash-cards\flash_cards.py" 
 
